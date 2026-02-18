29537e5fcf92c347b90051d514520c17c3545aba
toxicoutbrain <getnoise@gmail.com>
2026-02-18 13:14:36 +0900

    Ver0.9.5.3

--- src/constants.js
+++ src/constants.js
@@ -115,9 +115,9 @@ export const CONSTANTS = {
     // --- Sound System (SSOT) ---
     SOUND_ASSETS: {
         // BGM
-        BGM_STAGE_01: { type: "bgm", file: "assets/sound/bgm/bgm_stage_01.mp3", baseVolume: 0.3 },
-        BGM_STAGE_03: { type: "bgm", file: "assets/sound/bgm/bgm_stage_03.mp3", baseVolume: 0.4},
-        BGM_BOSS_01: { type: "bgm", file: "assets/sound/bgm/bgm_boss_01.mp3", baseVolume: 0.7 },
+        BGM_STAGE_01: { type: "bgm", file: "assets/sound/bgm/bgm_stage_01.mp3", baseVolume: 0.1 },
+        BGM_STAGE_03: { type: "bgm", file: "assets/sound/bgm/bgm_stage_03.mp3", baseVolume: 0.2},
+        BGM_BOSS_01: { type: "bgm", file: "assets/sound/bgm/bgm_boss_01.mp3", baseVolume: 0.3 },
 
         // SE
         SE_BARRIER_01: { type: "se", file: "assets/sound/se/se_barrier_01.mp3" },
